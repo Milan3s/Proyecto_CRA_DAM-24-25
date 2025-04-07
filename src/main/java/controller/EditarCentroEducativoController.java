23848950b4882e54fee14f93856f45bd0faaab69
milan3s <dmilanestrabajo@gmail.com>
2025-04-07 19:03:15 +0200

    Editar Centro Educativo limpio

---
+++ src/main/java/controller/EditarCentroEducativoController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Milanes
+ */
+public class EditarCentroEducativoController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
