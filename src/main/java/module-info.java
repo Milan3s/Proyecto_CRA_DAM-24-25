802e363ce6d819b7be06d370efbeeee50698f9fb
milan3s <dmilanestrabajo@gmail.com>
2025-04-07 22:32:33 +0200

    Clases actualizadas con Logger para depurar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,13 +7,13 @@ module main {
     // Abrimos los paquetes para que FXML pueda acceder a sus propiedades públicas
     opens controller to javafx.fxml, javafx.base;
     opens model to javafx.base;
-    opens utils to javafx.base; // ✅ Esto permite serializar logs si es necesario
+    opens utils to javafx.base; // Esto permite serializar logs si es necesario
 
     // Exportamos para uso desde otras partes del proyecto
     exports main;
     exports controller;
     exports model;
-    exports utils; // ✅ Si otras clases necesitan acceso al logger, por ejemplo
+    exports utils; // Si otras clases necesitan acceso al logger, por ejemplo
 
     requires de.jensd.fx.glyphs.fontawesome; // Para íconos FontAwesome (si lo usas en vistas)
 }
