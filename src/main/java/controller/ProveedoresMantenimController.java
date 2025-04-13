c657775d5bce3ae1fe351ec2136c7acbc0b62057
aortizortiz <11209457@alu.murciaeduca.es>
2025-04-14 00:04:13 +0200

    Proveedores_commit_3

--- src/main/java/controller/ProveedoresMantenimController.java
+++ src/main/java/controller/ProveedoresMantenimController.java
@@ -1,12 +1,21 @@
 package controller;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.Proveedor;
+import utils.DataBaseConection;
+import utils.LoggerUtils;
+import static utils.Utilidades.mostrarAlerta2;
 
 public class ProveedoresMantenimController implements Initializable {
 
@@ -50,5 +59,95 @@ public class ProveedoresMantenimController implements Initializable {
             txtTelefono.setText(proveedor.getTelefono());
             txtEmail.setText(proveedor.getEmail());
         }
+    }
+
+    @FXML
+    private void btnGuardarAction(ActionEvent event) {
+        if (null == this.proveedor) {
+            insertarProv();
+        } else {
+            actualizarProv();
+        }
+    }
+
+    @FXML
+    private void btnCancelarAction(ActionEvent event) {
+        cerrarVentana();
+    }
+    
+    private void cerrarVentana() {
+        Stage stage = (Stage) btnCancelar.getScene().getWindow();
+        stage.close();
+    }
+    
+    private void actualizarProv() {
+        int codProv = this.proveedor.getCodigo();
+        String nombre = txtNombre.getText().trim();
+        String calle = txtCalle.getText().trim();
+        String localidad = txtLocalidad.getText().trim();
+        String cp = txtCp.getText().trim();
+        String municipio = txtMunicipio.getText().trim();
+        String provincia = txtProvincia.getText().trim();
+        String telefono = txtTelefono.getText().trim();
+        String email = txtEmail.getText().trim();
+        
+        String sql = "UPDATE proveedores SET nombre = ?, calle = ?, localidad = ?, cp = ?, municipio = ?, provincia = ?, telefono = ?, email = ? WHERE codigo_proveedor = ?";
+        
+        try (Connection conn = DataBaseConection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, nombre);
+            stmt.setString(2, calle);
+            stmt.setString(3, localidad);
+            stmt.setString(4, cp);
+            stmt.setString(5, municipio);
+            stmt.setString(6, provincia);
+            stmt.setString(7, telefono);
+            stmt.setString(8, email);
+            stmt.setInt(9, codProv);
+            
+            int filas = stmt.executeUpdate();
+            
+            if (filas > 0) {
+                mostrarAlerta2("Éxito", "Proveedor actualizado correctamente.", Alert.AlertType.INFORMATION);
+                cerrarVentana();
+            }
+        } catch (SQLException e) {
+            mostrarAlerta2("Error SQL", "No se pudo actualizar el proveedor.\nDetalles: " + e.getMessage(), Alert.AlertType.ERROR);
+            LoggerUtils.logError("PROVEEDORES", "Error al ejecutar actualización de proveedor", e);
+        }
+    }
+    
+    private void insertarProv() {
+        String nombre = txtNombre.getText().trim();
+        String calle = txtCalle.getText().trim();
+        String localidad = txtLocalidad.getText().trim();
+        String cp = txtCp.getText().trim();
+        String municipio = txtMunicipio.getText().trim();
+        String provincia = txtProvincia.getText().trim();
+        String telefono = txtTelefono.getText().trim();
+        String email = txtEmail.getText().trim();
+        
+        String sql = "INSERT INTO proveedores (nombre, calle, localidad, cp, municipio, provincia, telefono, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        try (Connection conn = DataBaseConection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, nombre);
+            stmt.setString(2, calle);
+            stmt.setString(3, localidad);
+            stmt.setString(4, cp);
+            stmt.setString(5, municipio);
+            stmt.setString(6, provincia);
+            stmt.setString(7, telefono);
+            stmt.setString(8, email);
+            
+            int filas = stmt.executeUpdate();
+            
+            if (filas > 0) {
+                mostrarAlerta2("Éxito", "Proveedor guardado correctamente.", Alert.AlertType.INFORMATION);
+                cerrarVentana();
+            }
+        } catch (SQLException e) {
+            mostrarAlerta2("Error SQL", "No se pudo guardar el proveedor.\nDetalles: " + e.getMessage(), Alert.AlertType.ERROR);
+            LoggerUtils.logError("PROVEEDORES", "Error al ejecutar alta de proveedor", e);
+        }
+        
     }
 }
