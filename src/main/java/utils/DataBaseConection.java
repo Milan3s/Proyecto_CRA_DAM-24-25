3db98c85c9eadee5a044c4ed342d4d8f85d0196f
aortizortiz <11209457@alu.murciaeduca.es>
2025-04-17 23:39:59 +0200

    Proveedores_commit_7

--- src/main/java/utils/DataBaseConection.java
+++ src/main/java/utils/DataBaseConection.java
@@ -18,19 +18,12 @@ public class DataBaseConection {
     
     // Método para obtener la conexión
     public static Connection getConnection() {
-        //Connection connection = null;
         try {
-            // Cargar el driver de MySQL
-            //Class.forName("com.mysql.cj.jdbc.Driver");
-
             // Establecer la conexión
             if (connection == null) {
                 connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 System.out.println("Conexión exitosa a la base de datos.");
             }
-        //} catch (ClassNotFoundException e) {
-        //    System.err.println("Error: No se pudo cargar el driver de MySQL.");
-        //    e.printStackTrace();
         } catch (SQLException e) {
             System.err.println("Error: No se pudo conectar a la base de datos.");
             e.printStackTrace();
